0d54df427a8a6a3d57b484f41c543b08b6003534
Nyxnotz <newaleksandrstefancev6@gmail.com>
2025-12-15 00:59:50 +0200

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyBmH3-b1NzPDt4vhCaA89DLOCepKge5nag",
+    authDomain: "nyxchan-65a73.firebaseapp.com",
+    databaseURL: "https://nyxchan-65a73-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "nyxchan-65a73",
+    storageBucket: "nyxchan-65a73.firebasestorage.app",
+    messagingSenderId: "786040931352",
+    appId: "1:786040931352:web:454255ab6863f11c53af6d"
+  },
+  modPassword: "P7!dx92-LemFire"
+};
